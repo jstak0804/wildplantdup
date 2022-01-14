e37ed635f417bc606e310c9e8794755f9d3a676a
PieSignal <leeon@insiro.me>
2022-01-14 21:32:02 +0900

    add: eslint plugins

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'react/prop-types': ['off'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
