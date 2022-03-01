8a222ac8013474504db34a83ec2990a83c447669
Insiro <leeon@insiro.me>
2022-03-01 18:49:14 +0900

    change name

--- src/pages/Load/index.tsx
+++ src/pages/Load/index.tsx
@@ -82,7 +82,7 @@ const Loading: React.FC<Props> = ({ state }) => {
           type="primary"
           style={{ textAlign: 'center', marginLeft: '10px' }}
         >
-          분류 AI 실행
+          AI 실행
         </Button>
         <Card
           title="AI 실행 전 아래 항목을 선택해주세요"
